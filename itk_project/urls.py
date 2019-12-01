d8b65e1ff383c7f09a24d522552500d9b909b48e
RKNyra <rk.nyra@gmail.com>
2019-12-01 23:24:01 +0300

    create and populate the login template where users can access the form to log into the app

--- itk_project/urls.py
+++ itk_project/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('itk_app.urls')),
+    path('accounts/register/',RegistrationView.as_view(success_url='/accounts/login/')),
+    path('accounts/',include('django_registration.backends.one_step.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('logout/',views.LogoutView.as_view(),{'next_page':'/'}),
+
 ]
