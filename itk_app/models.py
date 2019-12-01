67da00eb290e839f21e3d00e0bdb4178c5609355
RKNyra <rk.nyra@gmail.com>
2019-12-02 00:16:32 +0300

    create a user/profile model that will be used to create the user/profile instances

--- itk_app/models.py
+++ itk_app/models.py
@@ -1,5 +1,20 @@
-from django.db import model
+from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from phone_field import PhoneField
+from pyuploadcare.dj.models import ImageField
+
+#User/Profile Model
+class Profile(models.Model):
+    prof_pic = ImageField(blank=True, manual_crop="", null=True)
+    bio = models.CharField(max_length = 250, null=True)
+    email = models.EmailField()
+    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField('auth.User',on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return str(self.user)
+    
+    def save_profile(self):
+        self.save()
